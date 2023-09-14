7422cd43703f183ccbbe23ed1f165ea37d8d08de
eugeniodevesch <eugenio@devesch.com.br>
2023-09-14 11:10:00 -0300

    fix

--- src/html/backoffice_create_plan/backoffice_create_plan.py
+++ src/html/backoffice_create_plan/backoffice_create_plan.py
@@ -13,6 +13,11 @@ class BackofficeCreatePlan(BackofficePage):
         html = super().parse_html()
 
         if self.post:
+            if self.post.get("plan_available_for_purchase"):
+                html.esc("plan_available_for_purchase_checked_val", "checked='checked'")
+            else:
+                html.esc("annually_div_visibility_val", "display:none;")
+
             if self.post.get("plan_available_annually"):
                 html.esc("plan_available_annually_checked_val", "checked='checked'")
             else:
@@ -29,6 +34,7 @@ class BackofficeCreatePlan(BackofficePage):
                 html.esc("monthly_div_visibility_val", "display:none;")
 
         else:
+            html.esc("plan_available_for_purchase_checked_val", "checked='checked'")
             html.esc("plan_available_annually_checked_val", "checked='checked'")
             html.esc("plan_available_monthly_checked_val", "checked='checked'")
             html.esc("plan_has_trial_checked_val", "checked='checked'")
