79a0fc91c7d1b24902a8ed4d5564fedfce8eab54
eugeniodevesch <eugenio@devesch.com.br>
2023-09-15 14:39:27 -0300

    fix

--- api/panel_get_address_data_with_zip.py
+++ api/panel_get_address_data_with_zip.py
@@ -1,4 +1,5 @@
 from python_web_frame.base_page import BasePage
+from utils.utils.Http import Http
 
 
 class PanelGetAddressDataWithZip(BasePage):
@@ -6,7 +7,7 @@ class PanelGetAddressDataWithZip(BasePage):
         # self.utils.send_payload_email(self.event, "CEP DATA")
         if not self.post.get("user_zip_code"):
             return {"error": "Nenhum código postal fornecido."}
-        api_cep_response = self.utils.get_request_address_data_with_zip_code(self.post["user_zip_code"])
+        api_cep_response = Http().get_request_address_data_with_zip_code(self.post["user_zip_code"])
         if not api_cep_response:
             return {"error": "Código postal inválido."}
         return {"success": api_cep_response}
--- api/panel_get_company_data_with_cnpj.py
+++ api/panel_get_company_data_with_cnpj.py
@@ -1,11 +1,12 @@
 from python_web_frame.base_page import BasePage
+from utils.utils.Http import Http
 
 
 class PanelGetCompanyDataWithCnpj(BasePage):
     def run(self):
         if not self.post.get("user_cnpj"):
             return {"error": "Nenhum CNPJ enviado no formulário."}
-        cnpj_address_response = self.utils.get_request_cnpj_address_data(self.post["user_cnpj"])
+        cnpj_address_response = Http().get_request_cnpj_address_data(self.post["user_cnpj"])
         if not cnpj_address_response:
             return {"error": "Não foi possível encontrar dados com o CNPJ informado."}
         return {"success": cnpj_address_response}
