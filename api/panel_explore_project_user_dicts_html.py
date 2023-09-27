bc85e110ae136c7dca2130706a9b18e3f0b92088
eugeniodevesch <eugenio@devesch.com.br>
2023-09-27 17:07:56 -0300

    fix

--- api/panel_explore_project_user_dicts_html.py
+++ api/panel_explore_project_user_dicts_html.py
@@ -3,4 +3,7 @@ from python_web_frame.panel_page import PanelPage
 
 class PanelExploreProjectUserDictsHtml(PanelPage):
     def run(self):
-        return {"success": self.list_html_user_folder_rows(self.user.get_user_actual_plan(), self.post.get("folder_id"), self.post.get("model_html"))}
+        user_plan = None
+        if self.user:
+            user_plan = self.user.get_user_actual_plan()
+        return {"success": self.list_html_user_folder_rows(user_plan, self.post.get("folder_id"), self.post.get("model_html"))}
