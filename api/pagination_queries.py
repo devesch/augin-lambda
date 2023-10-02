4ccb01996c092cd82c915531f638fd42710c4cb0
eugeniodevesch <eugenio@devesch.com.br>
2023-10-02 10:11:50 -0300

    fix

--- api/pagination_queries.py
+++ api/pagination_queries.py
@@ -19,32 +19,35 @@ class PaginationQueries(BackofficePage, PanelPage):
             user_orders = Dynamo().query_paginated_user_orders(self.user.user_id, self.user.user_total_orders_count, self.post["page"])
             return {"success": self.list_html_payment_history_rows(user_orders)}
 
-        if self.post["query"] == "query_paginated_all_coupons":
-            coupons = Dynamo().query_paginated_all_coupons(last_evaluated_key, limit=int(self.user.user_pagination_count))
-            html = self.list_html_backoffice_coupons_table_rows(coupons)
-            new_itens_count = str(len(coupons))
+        return getattr(self, self.post["query"])(last_evaluated_key)
 
-        if self.post["query"] == "query_paginated_all_last_login_users":
-            all_users = Dynamo().query_paginated_all_last_login_users(last_evaluated_key, limit=int(self.user.user_pagination_count))
-            html = self.list_html_backoffice_users_table_rows(all_users)
-            new_itens_count = str(len(all_users))
+    def query_paginated_all_coupons(self, last_evaluated_key):
+        coupons, last_evaluated_key = Dynamo().query_paginated_all_coupons(last_evaluated_key, limit=int(self.user.user_pagination_count))
+        html = self.list_html_backoffice_coupons_table_rows(coupons)
+        new_itens_count = str(len(coupons))
+        return {"success": html, "last_evaluated_key": last_evaluated_key, "new_itens_count": new_itens_count}
 
-        if self.post["query"] == "query_paginated_all_orders":
+    def query_paginated_all_last_login_users(self, last_evaluated_key):
+        all_users, last_evaluated_key = Dynamo().query_paginated_all_last_login_users(last_evaluated_key, limit=int(self.user.user_pagination_count))
+        html = self.list_html_backoffice_users_table_rows(all_users)
+        new_itens_count = str(len(all_users))
+        return {"success": html, "last_evaluated_key": last_evaluated_key, "new_itens_count": new_itens_count}
+
+    def query_paginated_all_orders(self, last_evaluated_key):
+        orders, last_evaluated_key = Dynamo().query_paginated_all_orders(last_evaluated_key, limit=int(self.user.user_pagination_count))
+        html = self.list_html_backoffice_orders_table_rows(orders)
+        new_itens_count = str(len(orders))
+        return {"success": html, "last_evaluated_key": last_evaluated_key, "new_itens_count": new_itens_count}
+
+    def query_paginated_all_orders_from_status(self, last_evaluated_key):
+        if not self.post.get("query_filter"):
+            return {"error": "no query_filter in post"}
+        if self.post["query_filter"] == "all":
             orders, last_evaluated_key = Dynamo().query_paginated_all_orders(last_evaluated_key, limit=int(self.user.user_pagination_count))
             html = self.list_html_backoffice_orders_table_rows(orders)
             new_itens_count = str(len(orders))
-
-        if self.post["query"] == "query_paginated_all_orders_from_status":
-            if not self.post.get("query_filter"):
-                return {"error": "no query_filter in post"}
-
-            if self.post["query_filter"] == "all":
-                orders, last_evaluated_key = Dynamo().query_paginated_all_orders(last_evaluated_key, limit=int(self.user.user_pagination_count))
-                html = self.list_html_backoffice_orders_table_rows(orders)
-                new_itens_count = str(len(orders))
-            else:
-                orders, last_evaluated_key = Dynamo().query_paginated_all_orders_from_status(self.post["query_filter"], last_evaluated_key, limit=int(self.user.user_pagination_count))
-                html = self.list_html_backoffice_orders_table_rows(orders)
-                new_itens_count = str(len(orders))
-
+        else:
+            orders, last_evaluated_key = Dynamo().query_paginated_all_orders_from_status(self.post["query_filter"], last_evaluated_key, limit=int(self.user.user_pagination_count))
+            html = self.list_html_backoffice_orders_table_rows(orders)
+            new_itens_count = str(len(orders))
         return {"success": html, "last_evaluated_key": last_evaluated_key, "new_itens_count": new_itens_count}
