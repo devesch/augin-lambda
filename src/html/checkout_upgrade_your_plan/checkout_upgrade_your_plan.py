e660b546be01f86c974a586c4627625fcbb40f9a
eugeniodevesch <eugenio@devesch.com.br>
2023-10-04 14:02:26 -0300

    fix

--- src/html/checkout_upgrade_your_plan/checkout_upgrade_your_plan.py
+++ src/html/checkout_upgrade_your_plan/checkout_upgrade_your_plan.py
@@ -72,7 +72,7 @@ class CheckoutUpgradeYourPlan(CheckoutPage):
 
                     if user_subscription and (user_subscription.get("subscription_plan_id") == plan["plan_id"]) and (user_subscription.get("subscription_recurrency") == recurrency):
                         html.esc("html_your_plan_button_or_upgrade_plan_button", self.show_html_your_plan_button())
-                    elif user_subscription and (user_subscription.get("subscription_plan_id") + "-" + user_subscription.get("subscription_recurrency") in plans_hierarchy) and (int(plans_hierarchy[user_subscription.get("subscription_plan_id") + "-" + user_subscription.get("subscription_recurrency")]) < int(plans_hierarchy[plan["plan_id"] + "-" + recurrency])):
+                    elif user_subscription and (user_subscription.get("subscription_plan_id") + "-" + user_subscription.get("subscription_recurrency") in plans_hierarchy) and (int(plans_hierarchy[user_subscription.get("subscription_plan_id") + "-" + user_subscription.get("subscription_recurrency")]) > int(plans_hierarchy[plan["plan_id"] + "-" + recurrency])):
                         html.esc("html_your_plan_button_or_upgrade_plan_button", self.show_html_upgrade_plan_button(plan["plan_id"], recurrency, "downgrade"))
                     else:
                         html.esc("html_your_plan_button_or_upgrade_plan_button", self.show_html_upgrade_plan_button(plan["plan_id"], recurrency, "upgrade"))
