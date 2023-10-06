fa1314f55f491917506374ebc0298da45d3423d7
eugeniodevesch <eugenio@devesch.com.br>
2023-10-06 13:58:21 -0300

    fix

--- objects/UploadedFile.py
+++ objects/UploadedFile.py
@@ -8,5 +8,6 @@ class UploadedFile:
         self.uploaded_file_id = uploaded_file_id
         self.uploaded_file_user_id = uploaded_file_user_id
         self.uploaded_file_post = uploaded_file_post
+        self.uploaded_file_response = {}
         self.created_at = str(time.time())
         self.entity = "uploaded_file"
