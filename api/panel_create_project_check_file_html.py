d99d7c8ded92f54f1a27a9344b0498b7551ad40e
eugeniodevesch <eugenio@devesch.com.br>
2023-10-06 14:02:21 -0300

    fix

--- api/panel_create_project_check_file_html.py
+++ api/panel_create_project_check_file_html.py
@@ -9,9 +9,9 @@ from utils.Config import lambda_constants
 class PanelCreateProjectCheckFileHtml(PanelPage):
     def run(self):
         uploaded_file = Dynamo().get_uploaded_file(self.post["uploaded_file_id"])
-        if not uploaded_file["response"]:
+        if not uploaded_file["uploaded_file_response"]:
             return {"keep_waiting": "Continue aguardando resposta"}
-        check_response = uploaded_file["response"]
+        check_response = uploaded_file["uploaded_file_response"]
         if "success" in check_response:
             check_response["success"]["message"] = self.translate(check_response["success"]["message"])
             check_response["success"]["file_formats_html"] = self.list_html_uploading_file_formats(check_response["success"]["file_formats"])
