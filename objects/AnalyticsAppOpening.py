7b6ca7e0d19b9017de18d3e74863ea52026e85f6
eugeniodevesch <eugenio@devesch.com.br>
2023-10-06 17:49:37 -0300

    app_opening_daily_amounts

---
+++ objects/AnalyticsAppOpening.py
@@ -0,0 +1,7 @@
+import time
+
+
+class AnalyticsAppOpening:
+    def __init__(self) -> None:
+        self.pk = "analytics_app_opening#"
+        self.sk = str(time.time())
--- src/html/backoffice_analytics/backoffice_analytics.py
+++ src/html/backoffice_analytics/backoffice_analytics.py
@@ -1,4 +1,5 @@
 ﻿from objects.AnalyticsNewUserRegistered import AnalyticsNewUserRegistered
+from objects.AnalyticsAppOpening import AnalyticsAppOpening
 from python_web_frame.backoffice_page import BackofficePage
 from utils.AWS.Dynamo import Dynamo
 from utils.utils.Date import Date
@@ -21,50 +22,65 @@ class BackofficeAnalytics(BackofficePage):
         days_ago_unix_delta = str(time.time() - (int(days_ago) * 86400))
 
         analytics_new_user_registereds = Dynamo().query_analytics("new_user_registered", days_ago_unix_delta)
-        analytics_new_user_registereds = self.add_dates_to_analytics_new_user_registereds(analytics_new_user_registereds)
+        analytics_app_openings = Dynamo().query_analytics("new_user_registered", days_ago_unix_delta)
+        analytics_new_user_registereds = self.add_dates_to_analytics(analytics_new_user_registereds)
+        analytics_app_openings = self.add_dates_to_analytics(analytics_app_openings)
 
-        new_user_registered_dates, new_user_registered_daily_amounts, new_user_registered_total_amounts = self.generate_analytics_new_user_registereds_data(analytics_new_user_registereds, days_ago)
+        dates = self.generate_dates(days_ago)
+
+        new_user_registered_daily_amounts, new_user_registered_total_amounts = self.generate_analytics_data(dates, analytics_new_user_registereds)
+        app_opening_daily_amounts, app_opening_total_amounts = self.generate_analytics_data(dates, analytics_app_openings)
 
         html.esc(days_ago + "_checked_val", 'selected="selected"')
-        html.esc("new_user_registered_dates_val", new_user_registered_dates)
+        html.esc("dates_val", dates)
+
         html.esc("new_user_registered_daily_amounts_val", new_user_registered_daily_amounts)
         html.esc("new_user_registered_total_amounts_val", new_user_registered_total_amounts)
-        # for x in range(50):
+
+        html.esc("app_opening_daily_amounts_val", app_opening_daily_amounts)
+        html.esc("app_opening_total_amounts_val", app_opening_total_amounts)
+
+        # for x in range(100):
         #     analytics_new_user_registered = AnalyticsNewUserRegistered().__dict__
         #     analytics_new_user_registered["sk"] = str(time.time() - random.randint(1, 31536000))
         #     Dynamo().put_entity(analytics_new_user_registered)
+
+        # for x in range(1000):
+        #     analytics_app_opening = AnalyticsAppOpening().__dict__
+        #     analytics_app_opening["sk"] = str(time.time() - random.randint(1, 31536000))
+        #     Dynamo().put_entity(analytics_app_opening)
 
         return str(html)
 
     def render_post(self):
         return self.render_get()
 
-    def generate_analytics_new_user_registereds_data(self, analytics_new_user_registereds, days_ago):
+    def generate_dates(self, days_ago):
         from datetime import datetime, timedelta
-
-        new_user_registered_dates = []
-        new_user_registered_daily_amounts = []
-        new_user_registered_total_amounts = []
 
         today = datetime.today()
         date_list = [(today - timedelta(days=x)).strftime("%d/%m/%y") for x in range(int(days_ago))]
-        new_user_registered_dates = date_list[::-1]
+        return date_list[::-1]
+
+    def generate_analytics_data(self, dates, analytics):
+        daily_amounts_list = []
+        total_amounts_list = []
 
         total_amounts = 0
-        for date in new_user_registered_dates:
+        for date in dates:
             daily_amounts = 0
-            for analytics in analytics_new_user_registereds:
-                if analytics["date"] == date:
+            for analytic in analytics:
+                if analytic["date"] == date:
                     daily_amounts += 1
 
             total_amounts += daily_amounts
-            new_user_registered_daily_amounts.append(daily_amounts)
-            new_user_registered_total_amounts.append(total_amounts)
+            daily_amounts_list.append(daily_amounts)
+            total_amounts_list.append(total_amounts)
 
-        return new_user_registered_dates, new_user_registered_daily_amounts, new_user_registered_total_amounts
+        return daily_amounts_list, total_amounts_list
 
-    def add_dates_to_analytics_new_user_registereds(self, analytics_new_user_registereds):
-        if analytics_new_user_registereds:
-            for analytics in analytics_new_user_registereds:
-                analytics["date"] = Date().format_unixtime_to_br_date(analytics["sk"])
-        return analytics_new_user_registereds
+    def add_dates_to_analytics(self, analytics):
+        if analytics:
+            for analytic in analytics:
+                analytic["date"] = Date().format_unixtime_to_br_date(analytic["sk"])
+        return analytics
