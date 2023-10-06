8a631dcf35e5bf38efa8b4fe0e6c1807a27a59d0
eugeniodevesch <eugenio@devesch.com.br>
2023-10-06 14:00:33 -0300

    fix

--- api/panel_create_project_check_file.py
+++ api/panel_create_project_check_file.py
@@ -11,9 +11,4 @@ class PanelCreateProjectCheckFile(PanelPage):
         uploaded_file = UploadedFile(Generate().generate_short_id(), self.user.user_id, self.post).__dict__
         Dynamo().put_entity(uploaded_file)
         Lambda().invoke(lambda_constants["lambda_check_model_uploaded_file"], "Event", {"uploaded_file_id": uploaded_file["uploaded_file_id"]})
-        return {"success": "upload_send_for_check"}
-        # check_response = ModelController().check_if_file_uploaded_is_valid(self.post["key"], self.post["original_name"], self.user)
-        # if "success" in check_response:
-        #     check_response["success"]["message"] = self.translate(check_response["success"]["message"])
-        #     check_response["success"]["file_formats_html"] = self.list_html_uploading_file_formats(check_response["success"]["file_formats"])
-        # return check_response
+        return {"success": uploaded_file["uploaded_file_id"]}
