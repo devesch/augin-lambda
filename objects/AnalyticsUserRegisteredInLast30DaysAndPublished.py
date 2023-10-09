8a3f5e255ea28da491ebbb93fd9aac6bf9cba50f
eugeniodevesch <eugenio@devesch.com.br>
2023-10-09 11:30:45 -0300

    fix

--- objects/AnalyticsUserRegisteredInLast30DaysAndPublished.py
+++ objects/AnalyticsUserRegisteredInLast30DaysAndPublished.py
@@ -1,3 +1,4 @@
+from utils.AWS.Dynamo import Dynamo
 import time
 
 
@@ -5,3 +6,7 @@ class AnalyticsUserRegisteredInLast30DaysAndPublished:
     def __init__(self) -> None:
         self.pk = "analytics_user_registered_in_last_30_days_and_published#"
         self.sk = str(time.time())
+
+
+def check_and_save_user_registered_in_last_30d_and_published_analytics():
+    Dynamo().put_entity(AnalyticsUserRegisteredInLast30DaysAndPublished().__dict__)
