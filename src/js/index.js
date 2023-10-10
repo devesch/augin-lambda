93431d2296a26231a031b5631267cee70f32c395
eugeniodevesch <eugenio@devesch.com.br>
2023-10-10 11:22:14 -0300

    updateUserUsedCloudSpaceInMbs

--- src/js/index.js
+++ src/js/index.js
@@ -665,23 +665,22 @@ export async function checkUploadModelFile(post_data) {
         let file_formats_div = document.getElementById("file_formats_div_" + post_data["element_index"]);
         file_formats_div.innerHTML = panel_create_project_check_file_html_response["success"]["file_formats_html"];
 
-        var update_user_response = await apiCaller("update_user", {
-            "command": "get_user_used_cloud_space_in_mbs",
-        });
 
-        let user_used_cloud_space_in_mbs_span = document.getElementById("user_used_cloud_space_in_mbs_span");
-        user_used_cloud_space_in_mbs_span.innerHTML = update_user_response["success"];
-
+        updateUserUsedCloudSpaceInMbs();
         checkIfCreateProjectSubmitButtonIsAvailable();
         checkIfCreateProjectIsFederated(false);
     }
 }
 
 
+export async function updateUserUsedCloudSpaceInMbs() {
+    var update_user_response = await apiCaller("update_user", {
+        "command": "get_user_used_cloud_space_in_mbs",
+    });
 
-
-
-
+    let user_used_cloud_space_in_mbs_span = document.getElementById("user_used_cloud_space_in_mbs_span");
+    user_used_cloud_space_in_mbs_span.innerHTML = update_user_response["success"];
+}
 
 export function checkIfCreateProjectIsFederated(checkbox = true) {
     let federated_switch_div = document.getElementById("federated_switch_div");
@@ -789,6 +788,8 @@ export async function deleteUploadingElement(index) {
         "command": "delete_model",
         "model_id": model_id_input.value
     });
+
+    updateUserUsedCloudSpaceInMbs();
 }
 
 
