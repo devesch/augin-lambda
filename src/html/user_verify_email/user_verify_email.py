1b695e1e276e12bae0a4fa64ed7a0068af005820
eugeniodevesch <eugenio@devesch.com.br>
2023-10-11 09:49:04 -0300

    loading user in post of verify email

--- src/html/user_verify_email/user_verify_email.py
+++ src/html/user_verify_email/user_verify_email.py
@@ -1,4 +1,5 @@
 from python_web_frame.user_page import UserPage
+from objects.User import User
 from utils.AWS.Dynamo import Dynamo
 from utils.utils.Http import Http
 from utils.utils.EncodeDecode import EncodeDecode
@@ -26,6 +27,7 @@ class UserVerifyEmail(UserPage):
         if self.path.get("change_email"):
             self.post["user_email"] = self.path["user_email"]
             self.generate_and_send_email_verification_code()
+
         return str(html)
 
     def render_post(self):
@@ -45,6 +47,8 @@ class UserVerifyEmail(UserPage):
                 return self.render_get_with_error("Código de verificação expirado.")
 
             if self.path.get("change_email"):
+                user = User()
+                user.load_information_with_auth_token(self.path["user_auth_token"])
                 return Http().redirect("user_email_changed_confirm/?user_auth_token=" + self.user.user_auth_token + "&new_user_email_encoded=" + EncodeDecode().encode_to_b64(self.path["user_email"]))
 
             return Http().redirect("user_register/?user_encoded_email=" + EncodeDecode().encode_to_b64(self.path["user_email"]) + "&verify_email_code=" + self.post["verify_email_code"])
