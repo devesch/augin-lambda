50ecd34723beefa4b8e23d9c591ad2cd82d13a45
eugeniodevesch <eugenio@devesch.com.br>
2023-10-13 15:28:19 -0300

    fix in check_if_user_can_upgrade_his_plan

--- api/update_user.py
+++ api/update_user.py
@@ -198,6 +198,8 @@ class UpdateUser(BasePage):
             return {"error": "Nenhum plano encontrado com este plan_id", "user_client_type": self.user.user_client_type}
         if not self.user.check_if_is_payment_ready():
             return {"error": "É necessário que o usuário atualize os seus dados", "user_client_type": self.user.user_client_type}
+        if float(self.user.user_address_data_last_update) < float(time.time() - 3600):
+            return {"error": "É necessário que o usuário confirme os seus dados", "user_client_type": self.user.user_client_type}
         else:
             return {"success": "O usuário pode trocar o seu plano atual"}
 
