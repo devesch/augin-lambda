d5127baf3367f532767e64eeac1474fd62c16528
matheusgdevesch <matheus@devesch.com.br>
2023-11-17 12:32:46 -0300

    FEAT: current selected plan

--- src/html/checkout_upgrade_your_plan/checkout_upgrade_your_plan.py
+++ src/html/checkout_upgrade_your_plan/checkout_upgrade_your_plan.py
@@ -73,8 +73,10 @@ class CheckoutUpgradeYourPlan(CheckoutPage):
 
                     if user_subscription and (user_subscription.get("subscription_plan_id") == plan["plan_id"]) and (user_subscription.get("subscription_recurrency") == recurrency) and float(user_subscription["subscription_valid_until"]) > time.time():
                         html.esc("html_your_plan_button_or_upgrade_plan_button", self.show_html_your_plan_button())
+                        html.esc("current_plan_active_val", "active")
                     elif user_subscription and (user_subscription.get("subscription_plan_id") + "-" + user_subscription.get("subscription_recurrency") in plans_hierarchy) and (int(plans_hierarchy[user_subscription.get("subscription_plan_id") + "-" + user_subscription.get("subscription_recurrency")]) > int(plans_hierarchy[plan["plan_id"] + "-" + recurrency])):
                         html.esc("html_your_plan_button_or_upgrade_plan_button", self.show_html_upgrade_plan_button(plan["plan_id"], recurrency, "downgrade"))
+                        html.esc("current_plan_active_val", "active")
                     else:
                         html.esc("html_your_plan_button_or_upgrade_plan_button", self.show_html_upgrade_plan_button(plan["plan_id"], recurrency, "upgrade"))
 
