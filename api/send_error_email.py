4247af583424dc3c138f8ee2fc344fad80215094
matheusgdevesch <matheus@devesch.com.br>
2023-12-05 09:58:33 -0300

    FIX: send error message

--- api/send_error_email.py
+++ api/send_error_email.py
@@ -1,5 +1,6 @@
 from python_web_frame.panel_page import PanelPage
 from utils.AWS.Ses import Ses
+from utils.Config import lambda_constants
 
 class SendErrorEmail(PanelPage):
     def run(self):
@@ -7,5 +8,5 @@ class SendErrorEmail(PanelPage):
             return {"error": "Nenhum erro enviado no post"}
         if not self.post.get("email_destination"):
             return {"error": "Nenhum email de destino enviado no post"}
-        Ses().send_error_email(event, lambda_constants["domain_name"] + str(event.get_prefix()) + " pages lambda", self.post["error"],  self.post["email_destination"])
+        Ses().send_error_email(self.event, lambda_constants["domain_name"] + str(event.get_prefix()) + " pages lambda", self.post["error"],  self.post["email_destination"])
         return {"success": "Email de erro enviado com sucesso"}
